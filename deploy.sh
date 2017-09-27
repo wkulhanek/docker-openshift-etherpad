ff6878a42195ab64b87566ad8920e45d4eae64f0
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-09-27 16:12:20 -0400

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 oc new-project etherpad --display-name "Shared Etherpad"
 oc new-app mysql-persistent --param MYSQL_USER=ether --param MYSQL_PASSWORD=ether --param MYSQL_DATABASE=ether --param VOLUME_CAPACITY=4Gi --param MYSQL_VERSION=5.7
-# Root Pwd: PfhiJw8Tmq55ju0d
-# DB Service Name: mysql
-# Connection URL: mysql://mysql:3306/
+sleep 15
+oc new-app wkulhanek/etherpad:latest -e DB_USER=ether -e DB_PASS=ether -e DB_DBID=ether -e DB_PORT=3306 -e DB_HOST=mysql
