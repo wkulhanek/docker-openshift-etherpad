ab5941e4578ffdd81259c6b6a16aec0769444d11
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-10-30 14:22:49 -0400

    Updated to use Template

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 oc new-project etherpad --display-name "Shared Etherpad"
 oc new-app mysql-persistent --param MYSQL_USER=ether --param MYSQL_PASSWORD=ether --param MYSQL_DATABASE=ether --param VOLUME_CAPACITY=4Gi --param MYSQL_VERSION=5.7
 sleep 15
-oc new-app wkulhanek/etherpad:latest -e DB_USER=ether -e DB_PASS=ether -e DB_DBID=ether -e DB_PORT=3306 -e DB_HOST=mysql
+oc new-app -f etherpad-template.yaml -p DB_USER=ether -p DB_PASS=ether -p DB_DBID=ether -p DB_PORT=3306 -p DB_HOST=mysql -p ADMIN_PASSWORD=secret
