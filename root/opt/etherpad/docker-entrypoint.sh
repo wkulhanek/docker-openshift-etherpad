f2fa13bdf71058b3116bd26abe6703ec5db42c11
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-10-30 13:26:12 -0400

    Created Etherpad Template, Moved configuration file (settings.json) into a ConfigMap, added a way to specify the Admin password for Etherpad

--- root/opt/etherpad/docker-entrypoint.sh
+++ root/opt/etherpad/docker-entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -eux 
+set -eux
 
 # If the database is not "linked" then an external database is being
 # used and the user is expected to set the following variables on the
 # docker command line:
-#   
+#
 #   DB_HOST - The hostname to reach for the db
 #   DB_DBID - The database name
 #   DB_PASS - The database password
 #   DB_PORT - The port where the db is listening
 #   DB_USER - The database username
 
-# If the database is "linked" (docker run --link) to this container 
+# If the database is "linked" (docker run --link) to this container
 # from a db container then translate the vars into ones that we will use.
 # NOTE: assumes linked alias is 'db'
 if [ ! -z "${DB_NAME:-}" ]; then
@@ -23,6 +23,10 @@ if [ ! -z "${DB_NAME:-}" ]; then
 fi
 
 # Update the settings.json with appropriate values
+# Having settings.json in its own config directory allows us to replace
+# it at runtime with a ConfigMap
+echo "Updating Settings File"
+cp config/settings.json .
 sed -i "s/DB_HOST/${DB_HOST}/" settings.json
 sed -i "s/DB_DBID/${DB_DBID}/" settings.json
 sed -i "s/DB_PASS/${DB_PASS}/" settings.json
