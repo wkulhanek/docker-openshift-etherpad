5a941446312d065d93d8fd9df8ebe1dec55af1ee
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2020-05-29 12:49:13 -0400

    Updated build to push latest

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export VERSION=1.8.4
 docker build . -t quay.io/wkulhanek/etherpad:${VERSION}
 docker tag quay.io/wkulhanek/etherpad:${VERSION} quay.io/wkulhanek/etherpad:latest
 docker push quay.io/wkulhanek/etherpad:${VERSION}
-#docker push quay.io/wkulhanek/etherpad:latest
+docker push quay.io/wkulhanek/etherpad:latest
 #git tag ${VERSION}
 #git push origin ${VERSION}
