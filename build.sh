dd7f63fed82676540f333c1f1bfe9d8ab6e2896a
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2020-06-29 12:27:58 -0400

    Updated build to push to team repo also, changed template to pull from team repo

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export VERSION=1.8.4
-docker build . -t quay.io/wkulhanek/etherpad:${VERSION}
-docker tag quay.io/wkulhanek/etherpad:${VERSION} quay.io/wkulhanek/etherpad:latest
+docker build . -t quay.io/gpte-devops-automation/etherpad:${VERSION}
+docker tag quay.io/gpte-devops-automation/etherpad:${VERSION} quay.io/gpte-devops-automation/etherpad:latest
+docker tag quay.io/gpte-devops-automation/etherpad:${VERSION} quay.io/wkulhanek/etherpad:${VERSION}
+docker tag quay.io/gpte-devops-automation/etherpad:${VERSION} quay.io/wkulhanek/etherpad:latest
+docker push quay.io/gpte-devops-automation/etherpad:${VERSION}
+docker push quay.io/gpte-devops-automation/etherpad:latest
 docker push quay.io/wkulhanek/etherpad:${VERSION}
 docker push quay.io/wkulhanek/etherpad:latest
 #git tag ${VERSION}
